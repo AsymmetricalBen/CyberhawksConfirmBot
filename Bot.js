c143512c2c9a3e30009324869f142de35c892e80
Benjamin Huckaba <huck.ben@gmail.com>
2020-10-12 20:36:15 -0400

    Commit

--- Bot.js
+++ Bot.js
@@ -6,18 +6,20 @@ require('dotenv').config();
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+const embedMessage = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Welcome to the Cyberhawks club server!')
+	.setDescription('For security reasons, in order to get fullaccess to the server channels please respond with your UNG email address here:');
+	
 //Regular expression for email 
 const emailRe = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 //Dictionary for storage of user tokens
 let userTokenDict = {};
 
-//Token generator
-var rand = function() {
-    return Math.random().toString(36).substr(2);
-}
+//Token generator fuction
 var tokenGen = function() {
-    return rand() + rand();
+    return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
 }
 
 //On discord Bot login
@@ -25,32 +27,48 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('guildMemberAdd',(member) => {
+    member.send(embedMessage);
+});
+
+
+
 //On message seen by Bot
 client.on('message', msg => {
+    if(msg.content.startsWith('!verifyForceAll')){
+        // TODO add method to index and force all users to verify their email address
+    }
 
-    let messageText = msg.content.toLowerCase();
+    if(msg.content.startsWith('!verify ')){
+        msg.member.send(embedMessage);
+    }
 
-    //if message was not made by bot, and matches regular expression for an email
-    if(msg.author.id != '762098796745719809' & 
-        emailRe.test(String(messageText).toLowerCase()) &
-        messageText.includes('@ung.edu')){
+    if(msg.channel.type == 'dm' & msg.author.id != '762098796745719809' ){
+        let messageText = msg.content
+    
+        if(emailRe.test(String(messageText).toLowerCase()) &
+            messageText.includes('@ung.edu')){
 
-            //if email text includes @ung.edu
-            console.log('message is a UNG email address, sending confirmation email.');
+                //if email text includes @ung.edu
+                console.log('message is a UNG email address, sending confirmation email.');
 
-            userToken = tokenGen();
-            console.log("sending token: " + userToken)
-            spawn('python', ['emailsender.py', messageText, userToken])
-            userTokenDict[userToken] = msg;
+                userToken = tokenGen();
+                console.log("sending token: " + userToken)
+                spawn('python', ['emailsender.py', messageText, userToken])
+                userTokenDict[userToken] = msg.author.id;
 
-            msg.reply('Please check your email to confirm that you are a UNG student');
-
+                msg.reply('Please check your email to confirm that you are a UNG student');
+            }
+            else{
+                console.log('message is not an email address, discarding.');
+                msg.reply('Please enter a valid UNG email address.');
+            }
         }
-        else{
-            console.log('message is not an email address, discarding.');
-            msg.reply('Please enter a valid UNG email address.');
+});
 
-        }
+//
+app.get('/*/test',  (req, res) => {
+    res.send('Good')
 });
 
 //On request to any suburl
@@ -62,13 +80,14 @@ app.get('/*', (req, res) => {
 
         //Try to get matching user from token
         try {
-            user = userTokenDict[token]
+            userID = userTokenDict[token]
+            user = client.guilds.cache.get('764197891001679872').members.cache.get(userID)
 
-            user.member.roles.add("764203726134050818")
-            console.log(user.author.username + ' confirmed their email, adding role')
+            user.roles.add("764203726134050818")
+            console.log(user.username + ' confirmed their email, adding role')
             res.send('Email Confirmed')
-            
-        } catch (error) {
+        } 
+        catch (error) {
             console.log('error invalid token attempted: ' + token)
             res.send('something went wrong.  Please try again later.')
         }        
