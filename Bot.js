d8618c205264aaed770e3f75b686dfbd53473995
Ben1729 <huck.ben@gmail.com>
2020-11-03 22:37:43 -0500

    Added comments for clairity

--- Bot.js
+++ Bot.js
@@ -1,24 +1,25 @@
+//Requires node modules child_process, discord.js, express, and dotenv
+
 const { spawn } = require('child_process');
 const Discord = require('discord.js');
 const app = require('express')();
 require('dotenv').config();
-
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
 
+//Message to be sent to users
 const embedMessage = new Discord.MessageEmbed()
     .setColor('#0099ff')
-    .setTitle('Welcome to the Cyberhawks club server!')
-	.setDescription('For security reasons, in order to get fullaccess to the server channels please respond with your UNG email address here:');
+    .setTitle('Welcome to the UNG Cyberhawks server!')
+	.setDescription('In order to get full access to the server channels please respond with your UNG email address here');
 	
 //Regular expression for email 
 const emailRe = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 //Dictionary for storage of user tokens
-let userTokenDict = {};
+var userTokenDict = {};
 
 //Token generator fuction
-var tokenGen = function() {
+const tokenGen = function() {
     return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
 }
 
@@ -27,72 +28,75 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+//On new user join, send DM prompting for verification
 client.on('guildMemberAdd',(member) => {
     member.send(embedMessage);
 });
 
-
-
 //On message seen by Bot
 client.on('message', msg => {
-    if(msg.content.startsWith('!verifyForceAll')){
-        // TODO add method to index and force all users to verify their email address
+
+    let messageText = msg.content
+
+    //Command used to force users without verrified role to verify their student email
+    if(messageText.startsWith('!verifyForceAll')){
+        // TODO add method to index and force all curently unauthized users to verify their email address
     }
 
-    if(msg.content.startsWith('!verify ')){
+    //Command used for single user to have verification message resent to themselfs 
+    if(messageText.startsWith('!verify')){
         msg.member.send(embedMessage);
     }
-
-    if(msg.channel.type == 'dm' & msg.author.id != '762098796745719809' ){
-        let messageText = msg.content
     
-        if(emailRe.test(String(messageText).toLowerCase()) &
-            messageText.includes('@ung.edu')){
+    //If message is DM, passes email RE check, and includes @ung.edu
+    if(msg.channel.type == 'dm' & emailRe.test(String(messageText).toLowerCase()) & messageText.includes('@ung.edu')) {
 
-                //if email text includes @ung.edu
-                console.log('message is a UNG email address, sending confirmation email.');
+            //if email text includes @ung.edu
+            console.log('message is a UNG email address, sending confirmation email.');
 
-                userToken = tokenGen();
-                console.log("sending token: " + userToken);
-                spawn('python', ['emailsender.py', messageText, userToken]);
-                userTokenDict[userToken] = msg.author.id;
+            //Generate token
+            userToken = tokenGen();
 
-                msg.reply('Please check your email to confirm that you are a UNG student');
-            }
-            else{
-                console.log('message is not an email address, discarding.');
-                msg.reply('Please enter a valid UNG email address.');
-            }
+            //Send email with link containing token
+            spawn('python', ['emailsender.py', messageText, userToken]);
+            console.log("sending token: " + userToken);
+
+            //Store token to dictionary for later
+            //I don't particularly like using a dictionary here, but for the time being it works as a proof of concept
+            userTokenDict[userToken] = msg.author.id;
+
+            msg.reply('Please check your email to confirm that you are a UNG student');
         }
-});
+        else if (msg.channel.type == 'dm' & msg.author.id != '762098796745719809'){
 
-//
-app.get('/*/test',  (req, res) => {
-    res.send('Good');
+            console.log('message is not a UNG email address, discarding.');
+            msg.reply('Please enter a valid UNG email address.');
+        }
 });
 
 //On request to any suburl
 app.get('/*', (req, res) => {
-    if(req.originalUrl != '/favicon.ico'){
 
-        //Get token from suburl
-        token = req.originalUrl.replace('/', '');
+    //Get token from suburl
+    token = req.originalUrl.replace('/', '');
 
-        //Try to get matching user from token
-        try {
-            userID = userTokenDict[token];
-            user = client.guilds.cache.get('764197891001679872').members.cache.get(userID);
+    //Try to get matching user from token
+    try {
+        userID = userTokenDict[token];
+        user = client.guilds.cache.get('764197891001679872').members.cache.get(userID);
 
-            user.roles.add("764203726134050818");
-            console.log(user.username + ' confirmed their email, adding role');
-            res.send('Email Confirmed');
-        } 
-        catch (error) {
-            console.log('error invalid token attempted: ' + token);
-            res.send('something went wrong.  Please try again later.');
-        }        
-    }
+        user.roles.add("764203726134050818");
+        console.log(user.username + ' confirmed their email, adding role');
+        res.send('Email Confirmed');
+    } 
+    catch (error) {
+        console.log('error invalid token attempted: ' + token);
+        res.send('something went wrong.  Please try again later.');
+    }        
 });
 
+//Discord bot login token should be in a .env file containing DISCORD_TOKEN="{TOKEN HERE}"
 client.login();
+
+//Hosting on port 3000 for testing
 app.listen(3000, console.log('server starting on 3000'));
--- emailSender.py
+++ emailSender.py
@@ -1,14 +1,18 @@
 import smtplib, ssl, sys, os
 from dotenv import load_dotenv
 
+
+#This is a work in progress and will likely be switched to use the office365 package
+#At the moment SMTP does not seem like it will work for us to send emails, so the next alternative is through Azure
+
 load_dotenv()
-SENDER_EMAIL = "cyberhawkconfirm@gmail.com"
+SENDER_EMAIL = "cyberhawks@ung.edu"
 RECIEVER_EMAIL = sys.argv[1]
 TOKEN = sys.argv[2]
 PASSWORD = os.environ.get('EMAIL_PASSWORD')
 IP = os.environ.get('IP')
 PORT = 587
-SMTP_SERVER = "smtp.gmail.com"
+SMTP_SERVER = "mxa-00220c02.gslb.pphosted.com"
 CONTEXT = ssl.create_default_context()
 
 try:
@@ -22,4 +26,5 @@ try:
 except Exception as e:
     print(e)
 finally:
+    print(1)
     server.quit() 
