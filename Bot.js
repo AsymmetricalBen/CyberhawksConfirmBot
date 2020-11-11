cbaae278941d9ae1d845cf14dc956a7c5ea9ba42
Ben1729 <huck.ben@gmail.com>
2020-11-11 13:43:16 -0500

    Commit

--- Bot.js
+++ Bot.js
@@ -8,9 +8,9 @@ const client = new Discord.Client();
 
 //Message to be sent to users
 const embedMessage = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Welcome to the UNG Cyberhawks server!')
-	.setDescription('In order to get full access to the server channels please respond with your UNG email address here');
+    .setColor(process.env.MESSAGE_COLOR)
+    .setTitle(process.env.MESSAGE_TITLE)
+	.setDescription(process.env.MESSAGE_TEXT);
 	
 //Regular expression for email 
 const emailRe = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -18,14 +18,30 @@ const emailRe = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-
 //Dictionary for storage of user tokens
 var userTokenDict = {};
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 //Token generator fuction
 const tokenGen = () => {
     return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
 }
 
+
+const emailSender = spawn('python', ['emailsenderv2.py']);
+
+emailSender.stdout.on('data', (data) => {
+    console.log(data.toString());
+});
+
+emailSender.stderr.on('data', (data) => {
+    console.error(data.toString());
+});
+
 //On discord Bot login
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 //On new user join, send DM prompting for verification
@@ -48,8 +64,8 @@ client.on('message', msg => {
         msg.member.send(embedMessage);
     }
     
-    //If message is DM, passes email RE check, and includes @ung.edu
-    if(msg.channel.type == 'dm' & emailRe.test(String(messageText).toLowerCase()) & messageText.includes('@ung.edu')) {
+    //If message is DM, passes email RE check, includes @ung.edu, and is 18 chars long
+    if(msg.channel.type == 'dm' & emailRe.test(String(messageText).toLowerCase()) & messageText.includes('@ung.edu') & messageText.length == 18) {
 
             //if email text includes @ung.edu
             console.log('Message is a UNG email address, sending confirmation email.');
@@ -58,25 +74,32 @@ client.on('message', msg => {
             userToken = tokenGen();
 
             //Send email with link containing token
-            spawn('python', ['emailsender.py', messageText, userToken]);
+            emailSender.stdin.write(messageText.toLowerCase() + ' ' + userToken + '\n');
             console.log("Sending token: " + userToken);
 
             //Store token to dictionary for later
             //I don't particularly like using a dictionary here, but for the time being it works as a proof of concept
             userTokenDict[userToken] = msg.author.id;
 
-            msg.reply('Please check your email to confirm that you are a UNG student');
+            msg.reply(process.env.GOOD_EMAIL);
         }
         else if (msg.channel.type == 'dm' & msg.author.id != '762098796745719809'){
 
             console.log('Message is not a UNG email address, discarding.');
-            msg.reply('Please enter a valid UNG email address.');
+            msg.reply(process.env.BAD_EMAIL);
         }
+});
+
+//
+app.get('/favicon.ico', (req, res) => {
+    res.send('icon.jpg')
 });
 
 //On request to any suburl
 app.get('/*', (req, res) => {
+    if(req.originalUrl == '/favicon.ico'){
 
+    }
     //Get token from suburl
     token = req.originalUrl.replace('/', '');
 
@@ -99,8 +122,14 @@ app.get('/*', (req, res) => {
     }        
 });
 
+
 //Discord bot login token should be in a .env file containing DISCORD_TOKEN="{TOKEN HERE}"
 client.login();
 
 //Hosting on port 3000 for testing
 app.listen(3000, console.log('server starting on 3000'));
+
+readline.question('', url => {
+    emailSender.stdin.write(url + '\n');
+    readline.close();
+});
---
+++ emailSenderv2.py
@@ -0,0 +1,31 @@
+import sys, os
+from O365 import Account
+from dotenv import load_dotenv
+
+load_dotenv()
+CLIENT_ID = os.environ.get('EMAIL_ID')
+CLIENT_SECRET = os.environ.get('EMAIL_SECRET')
+TENANT_ID = os.environ.get('TENANT_ID')
+SERVER_IP = os.environ.get('SERVER_IP')
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+
+credentials = (CLIENT_ID, CLIENT_SECRET)
+account = Account(credentials, tenant_id=TENANT_ID)#auth_flow_type='credentials', 
+if account.authenticate(scopes=['https://graph.microsoft.com/.default']): 
+    
+    while True:
+        email, token = input().split()
+        mailbox = account.mailbox('EMAIL_ADDRESS')
+        m = mailbox.new_message()
+        m.to.add(email)
+        m.subject = 'Cyberhawks Email Confirmation Test!'
+        m.body = """
+                <html>
+                    <body>
+                        Please <a href=\"http://""" + str(SERVER_IP) + "/" + str(token) + """\">follow this link</a> to confirm your discord account.
+                            
+                        </p>
+                    </body>
+                </html>
+                """
+        m.send()
